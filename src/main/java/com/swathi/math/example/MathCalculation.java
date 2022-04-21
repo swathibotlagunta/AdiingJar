2e201250c20ccfdfd8a8d979f8b397d81d8f72be
Luckii59 <98205344+Luckii59@users.noreply.github.com>
2022-04-21 11:45:38 +0530

    FirstCommit

---
+++ src/main/java/com/swathi/math/example/MathCalculation.java
@@ -0,0 +1,20 @@
+package com.swathi.math.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.swathi.Arithoper.function.Operation;
+
+@SpringBootApplication
+public class MathCalculation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathCalculation.class, args);
+	
+          System.out.println("hi");
+          System.out.println("10 + 10 : " +(Operation.add(10,10)));
+          
+}
+	
+}
+
---
+++ src/test/java/com/swathi/math/example/MathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swathi.math.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
